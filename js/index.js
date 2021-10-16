4d7e59fda84eb450903df2ea5df936e2a826d1a7
Barox Phan <pqb.barox@gmail.com>
2021-10-16 19:53:52 +0700

    Update

--- js/index.js
+++ js/index.js
@@ -1,40 +1,43 @@
-import { toggleSomething, checkShowMenu } from'./utils/utils.js';
+import { toggleSomething, checkShowMenu, hideNavbar } from "./utils/utils.js";
 
-const navbarBtn = document.querySelector('.btn-navbar');
-const navbarMenu = document.querySelector('.navbar-menu');
-const navbarLinks = document.querySelectorAll('.navbar-link');
-const btnTabs = document.querySelectorAll('.btn-tab');
-const dynamicLine = document.querySelector('.dynamic-line');
-const descTabs = document.querySelectorAll('.tab-desc');
+const navbarBtn = document.querySelector(".btn-navbar");
+const navbarMenu = document.querySelector(".navbar-menu");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const btnTabs = document.querySelectorAll(".btn-tab");
+const dynamicLine = document.querySelector(".dynamic-line");
+const descTabs = document.querySelectorAll(".tab-desc");
 toggleSomething({
-    toggles: [navbarBtn],
-    targetsChange: navbarMenu,
-    classChanges: ['show-menu']
+  toggles: [navbarBtn],
+  targetsChange: navbarMenu,
+  classChanges: ["show-menu"],
 });
 
-navbarBtn.addEventListener('click', checkShowMenu);
+navbarBtn.addEventListener("click", checkShowMenu);
 
+btnTabs.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let item = e.target;
+    let left = item.offsetLeft;
+    let width = item.offsetWidth;
+    let dataIndex = item.dataset.index;
+    let descTab = document.querySelector(
+      `.tab-desc[data-index="${dataIndex}"]`
+    );
 
-btnTabs.forEach(btn => {
-    btn.addEventListener('click', function(e) {
-        let item = e.target;
-        console.dir(e.target)
-        let left = item.offsetLeft;
-        let width = item.offsetWidth;
-        console.log(left,width);
-        let dataIndex = item.dataset.index;
-        let descTab = document.querySelector(`.tab-desc[data-index="${dataIndex}"]`);
-        resetActive(btnTabs);
-        resetActive(descTabs);
-        item.classList.add('active');
-        descTab.classList.add('active');
-        dynamicLine.style = `left: ${left}px; width:${width}px`;
-    });
+    resetActive(btnTabs);
+    resetActive(descTabs);
+    item.classList.add("active");
+    descTab.classList.add("active");
+    dynamicLine.style = `left: ${left}px; width:${width}px`;
+  });
 });
 
+navbarLinks.forEach((navbarLink) => {
+  navbarLink.addEventListener("click", hideNavbar);
+});
 
-function resetActive (listElm) {
-    listElm.forEach(elm => {
-        elm.classList.remove('active');
-    })
+function resetActive(listElm) {
+  listElm.forEach((elm) => {
+    elm.classList.remove("active");
+  });
 }
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,45 +1,53 @@
-function  toggleSomething({toggles,targetsChange,classChanges}) {
-    toggles.forEach((toggle, index) => {
-        toggle.addEventListener('click', () => {
-            checkShowMenu();
-        targetsChange.style.display = 'block';
-        setTimeout(()=> {
-            let condition1 = targetsChange.classList.contains(classChanges[index]);
-            let condition2 = targetsChange.classList.contains(classChanges[0])
+function toggleSomething({ toggles, targetsChange, classChanges }) {
+  toggles.forEach((toggle, index) => {
+    toggle.addEventListener("click", () => {
+      checkShowMenu();
+      targetsChange.style.display = "block";
+      setTimeout(() => {
+        let condition1 = targetsChange.classList.contains(classChanges[index]);
+        let condition2 = targetsChange.classList.contains(classChanges[0]);
 
-            if (condition1 || condition2) {
-                setTimeout(() => {targetsChange.style.display = 'none'},300)
-            }
-            
-            targetsChange.classList.toggle(classChanges[index] || classChanges[0]);
-        },0);
-    })});
+        if (condition1 || condition2) {
+          setTimeout(() => {
+            targetsChange.style.display = "none";
+          }, 300);
+        }
+
+        targetsChange.classList.toggle(classChanges[index] || classChanges[0]);
+      }, 0);
+    });
+  });
 }
 
-function changeSomethingOnScroll({elements, offsets, classChanges}) {
-elements.forEach((elm,index) => {
+function changeSomethingOnScroll({ elements, offsets, classChanges }) {
+  elements.forEach((elm, index) => {
     if (this.scrollY >= offsets[index]) {
-        elm.classList.add(classChanges[index]);
-        return;
+      elm.classList.add(classChanges[index]);
+      return;
     }
 
     elm.classList.remove(classChanges[index]);
-})
-
+  });
 }
-
 
 function checkShowMenu() {
-    const navbarBtn = document.querySelector('.btn-navbar');
-    const navbarMenu = document.querySelector('.navbar-menu');
-    
-    if (!navbarMenu.classList.contains('show-menu')) {
+  const navbarBtn = document.querySelector(".btn-navbar");
+  const navbarMenu = document.querySelector(".navbar-menu");
 
-        navbarBtn.innerHTML = '<i class="ri-close-line"></i>';
-        return;
-    }
-
-    navbarBtn.innerHTML = '<i class="ri-apps-2-line"></i>';
+  if (!navbarMenu.classList.contains("show-menu")) {
+    navbarBtn.innerHTML = '<i class="ri-close-line"></i>';
+    return;
+  }
+  navbarBtn.innerHTML = '<i class="ri-apps-2-line"></i>';
 }
 
-export { toggleSomething, changeSomethingOnScroll, checkShowMenu};
+function hideNavbar() {
+  const navbarBtn = document.querySelector(".btn-navbar");
+  const navbar = document.querySelector(".navbar-menu");
+
+  navbar.classList.remove("show-menu");
+  navbar.removeAttribute("style");
+  navbarBtn.innerHTML = '<i class="ri-apps-2-line"></i>';
+}
+
+export { toggleSomething, changeSomethingOnScroll, checkShowMenu, hideNavbar };
